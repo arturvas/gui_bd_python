7907c323884b23f33aeb48e18affbfd04ed597f2
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-09-06 20:59:53 -0400

    interface com label e entrys

--- main.py
+++ main.py
@@ -23,3 +23,39 @@ conexao.commit()
 # fecha a conexão com o banco de dados SQLite
 conexao.close()
 """
+
+
+# criação da interface gráfica
+janela = tk.Tk()
+janela.title('Ferramenta de Cadastro de Clientes')
+
+# Labels
+label_nome = tk.Label(janela, text='Nome')
+label_nome.grid(row=0, column=0, padx=10, pady=10)
+
+label_sobrenome = tk.Label(janela, text='Sobrenome')
+label_sobrenome.grid(row=1, column=0, padx=10, pady=10)
+
+label_email = tk.Label(janela, text='E-mail')
+label_email.grid(row=2, column=0, padx=10, pady=10)
+
+label_telefone = tk.Label(janela, text='Telefone')
+label_telefone.grid(row=3, column=0, padx=10, pady=10)
+
+
+# Entrys:
+
+entry_nome = tk.Entry(janela, text='Nome', width=30)
+entry_nome.grid(row=0, column=1, padx=10, pady=10)
+
+entry_sobrenome = tk.Entry(janela, text='Sobrenome', width=30)
+entry_sobrenome.grid(row=1, column=1, padx=10, pady=10)
+
+entry_email = tk.Entry(janela, text='E-mail', width=30)
+entry_email.grid(row=2, column=1, padx=10, pady=10)
+
+entry_telefone = tk.Entry(janela, text='Telefone', width=30)
+entry_telefone.grid(row=3, column=1, œpadx=10, pady=10)
+
+
+janela.mainloop()
