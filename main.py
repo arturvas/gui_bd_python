9cbc8f4e1727c1c32e737ca82b3354f52d84d55e
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-09-06 22:36:23 -0400

    add `.gitignore`

--- main.py
+++ main.py
@@ -55,7 +55,15 @@ entry_email = tk.Entry(janela, text='E-mail', width=30)
 entry_email.grid(row=2, column=1, padx=10, pady=10)
 
 entry_telefone = tk.Entry(janela, text='Telefone', width=30)
-entry_telefone.grid(row=3, column=1, œpadx=10, pady=10)
+entry_telefone.grid(row=3, column=1, padx=10, pady=10)
 
+
+# Botões
+
+botao_cadastrar = tk.Button(janela, text='Cadastrar Cliente', command= cadastrar_clientes)
+botao_cadastrar.grid(row=4, column=0, padx=10, pady=10)
+
+botao_exportar = tk.Button(janela, text='Exportar Base de Clientes', command= exporta_clientes)
+botao_exportar.grid(row=5, column=0, padx=10, pady=10)
 
 janela.mainloop()
