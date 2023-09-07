5a888565d76971efb331fe20a1219233a609e398
Artur Vasconcelos <artur.vasconcelos8@gmail.com>
2023-09-07 00:15:50 -0400

    feat:criar bd

--- main.py
+++ main.py
@@ -2,27 +2,27 @@ import tkinter as tk
 import sqlite3
 import pandas as pd
 
-"""
-conexao = sqlite3.connect('banco_clientes.db')
 
-# O cursor é usado para executar comandos SQL no banco de dados
-c = conexao.cursor()
+def criar_tabela():
+    conexao = sqlite3.connect('banco_clientes.db')
 
-# executar uma ação dentro do banco
-c.execute('''CREATE TABLE clientes (
-    nome text,
-    sobrenome text,
-    email text,
-    telefone text
-    )
-''')
+    # O cursor é usado para executar comandos SQL no banco de dados
+    c = conexao.cursor()
 
-# commit (confirmação) das alterações no banco de dados
-conexao.commit()
+    # executar uma ação dentro do banco
+    c.execute('''CREATE TABLE clientes (
+        nome text,
+        sobrenome text,
+        email text,
+        telefone text
+        )
+    ''')
 
-# fecha a conexão com o banco de dados SQLite
-conexao.close()
-"""
+    # commit (confirmação) das alterações no banco de dados
+    conexao.commit()
+
+    # fecha a conexão com o banco de dados SQLite
+    conexao.close()
 
 
 def cadastrar_cliente():
@@ -34,10 +34,10 @@ def cadastrar_cliente():
     # executar uma ação dentro do banco
     c.execute('INSERT INTO clientes VALUES (:nome, :sobrenome, :email, :telefone)',
               {
-                  'nome':entry_nome.get(),
-                  'sobrenome':entry_sobrenome.get(),
-                  'email':entry_email.get(),
-                  'telefone':entry_telefone.get()
+                  'nome': entry_nome.get(),
+                  'sobrenome': entry_sobrenome.get(),
+                  'email': entry_email.get(),
+                  'telefone': entry_telefone.get()
               })
 
     # commit (confirmação) das alterações no banco de dados
@@ -54,7 +54,28 @@ def cadastrar_cliente():
 
 
 def exportar_clientes():
-    pass
+    conexao = sqlite3.connect('banco_clientes.db')
+
+    # O cursor é usado para executar comandos SQL no banco de dados
+    c = conexao.cursor()
+
+    # executar uma ação dentro do banco
+    c.execute('SELECT *, oid FROM clientes')
+
+    # Pegar todos os dados selecionados e trazer o valor para a variável
+    clientes_cadastrados = c.fetchall()
+
+    # coloca esses dados em um DataFrame pandas
+    clientes_cadastrados = pd.DataFrame(clientes_cadastrados, columns=['nome', 'sobrenome', 'email', 'telefone', 'id_banco'])
+
+    # exporta para um arquivo em Excel
+    clientes_cadastrados.to_excel('banco_clientes.xlsx')
+
+    # commit (confirmação) das alterações no banco de dados
+    conexao.commit()
+
+    # fecha a conexão com o banco de dados SQLite
+    conexao.close()
 
 
 # criação da interface gráfica
@@ -63,39 +84,42 @@ janela.title('Ferramenta de Cadastro de Clientes')
 
 # Labels
 label_nome = tk.Label(janela, text='Nome')
-label_nome.grid(row=0, column=0, padx=10, pady=10)
+label_nome.grid(row=1, column=0, padx=10, pady=10)
 
 label_sobrenome = tk.Label(janela, text='Sobrenome')
-label_sobrenome.grid(row=1, column=0, padx=10, pady=10)
+label_sobrenome.grid(row=2, column=0, padx=10, pady=10)
 
 label_email = tk.Label(janela, text='E-mail')
-label_email.grid(row=2, column=0, padx=10, pady=10)
+label_email.grid(row=3, column=0, padx=10, pady=10)
 
 label_telefone = tk.Label(janela, text='Telefone')
-label_telefone.grid(row=3, column=0, padx=10, pady=10)
+label_telefone.grid(row=4, column=0, padx=10, pady=10)
 
 
-# Entrys:
+# Entry:
 
-entry_nome = tk.Entry(janela, text='Nome', width=30)
-entry_nome.grid(row=0, column=1, padx=10, pady=10)
+entry_nome = tk.Entry(janela, width=30)
+entry_nome.grid(row=1, column=1, padx=10, pady=10)
 
-entry_sobrenome = tk.Entry(janela, text='Sobrenome', width=30)
-entry_sobrenome.grid(row=1, column=1, padx=10, pady=10)
+entry_sobrenome = tk.Entry(janela, width=30)
+entry_sobrenome.grid(row=2, column=1, padx=10, pady=10)
 
-entry_email = tk.Entry(janela, text='E-mail', width=30)
-entry_email.grid(row=2, column=1, padx=10, pady=10)
+entry_email = tk.Entry(janela, width=30)
+entry_email.grid(row=3, column=1, padx=10, pady=10)
 
-entry_telefone = tk.Entry(janela, text='Telefone', width=30)
-entry_telefone.grid(row=3, column=1, padx=10, pady=10)
+entry_telefone = tk.Entry(janela, width=30)
+entry_telefone.grid(row=4, column=1, padx=10, pady=10)
 
 
 # Botões
 
-botao_cadastrar = tk.Button(janela, text='Cadastrar Cliente', command=cadastrar_cliente)
-botao_cadastrar.grid(row=4, column=0, padx=10, pady=10, columnspan=2, ipadx=80)
+btn_novaTabela = tk.Button(janela, text='Criar Nova Tabela', command=criar_tabela)
+btn_novaTabela.grid(row=0, column=0, padx=10, pady=10, columnspan=2, ipadx=75)
 
-botao_exportar = tk.Button(janela, text='Exportar Base de Clientes', command=exportar_clientes)
-botao_exportar.grid(row=5, column=0, padx=10, pady=10, columnspan=2, ipadx=54)
+btn_cadastrar = tk.Button(janela, text='Cadastrar Cliente', command=cadastrar_cliente)
+btn_cadastrar.grid(row=5, column=0, padx=10, pady=10, columnspan=2, ipadx=80)
+
+btn_exportar = tk.Button(janela, text='Exportar Base de Clientes', command=exportar_clientes)
+btn_exportar.grid(row=6, column=0, padx=10, pady=10, columnspan=2, ipadx=54)
 
 janela.mainloop()
